a74ab47595bd9b48228481d833b5deaf0e272449
azurepolarbear <azurepolarbear@users.noreply.github.com>
2025-02-03 12:10:39 -0600

    Add line categories from wallpapers project.

---
+++ src/main/lines/line-categories.ts
@@ -0,0 +1,11 @@
+export enum LineDensity {
+    LOW = 'low',
+    MEDIUM = 'medium',
+    HIGH = 'high'
+}
+
+export enum LineThickness {
+    THIN = 'thin',
+    MEDIUM = 'medium',
+    THICK = 'thick'
+}
